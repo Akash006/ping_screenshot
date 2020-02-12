1457b4a3a741d31c9663bb78c5e6a448d5d7e302
Akash006 <mastermindvishu97@gmail.com>
2020-02-13 00:23:08 +0530

    Adding html email template.

---
+++ html_email.py
@@ -0,0 +1,236 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase 
+from email import encoders
+
+mail_content = '''Hi Team,
+
+This is just a test HTML mail.
+
+-- 
+Thanks & regards,
+Akash saini | Globallogic
+'''
+
+#The mail addresses and password
+sender_address = 'to@gmail.com'
+sender_pass = 'password'
+receiver_address = 'from@gmail.com'
+
+#Setup the MIME
+message = MIMEMultipart()
+message['From'] = sender_address
+message['To'] = receiver_address
+message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
+
+#The body and the attachments for the mail
+html = """\
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Table V04</title>
+</head>
+<body>
+					<div>
+						<table border="1">
+							<tbody>
+							<tr class="row100 head">
+									<th class="cell100 column1">Class name</th>
+									<th class="cell100 column2">Type</th>
+									<th class="cell100 column3">Hours</th>
+									<th class="cell100 column4">Trainer</th>
+									<th class="cell100 column5">Spots</th>
+								</tr>
+								<tr class="row100 body">
+									<td class="cell100 column1">Like a butterfly</td>
+									<td class="cell100 column2">Boxing</td>
+									<td class="cell100 column3">9:00 AM - 11:00 AM</td>
+									<td class="cell100 column4">Aaron Chapman</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Mind & Body</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">8:00 AM - 9:00 AM</td>
+									<td class="cell100 column4">Adam Stewart</td>
+									<td class="cell100 column5">15</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Crit Cardio</td>
+									<td class="cell100 column2">Gym</td>
+									<td class="cell100 column3">9:00 AM - 10:00 AM</td>
+									<td class="cell100 column4">Aaron Chapman</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Wheel Pose Full Posture</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">7:00 AM - 8:30 AM</td>
+									<td class="cell100 column4">Donna Wilson</td>
+									<td class="cell100 column5">15</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Playful Dancer's Flow</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">8:00 AM - 9:00 AM</td>
+									<td class="cell100 column4">Donna Wilson</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Zumba Dance</td>
+									<td class="cell100 column2">Dance</td>
+									<td class="cell100 column3">5:00 PM - 7:00 PM</td>
+									<td class="cell100 column4">Donna Wilson</td>
+									<td class="cell100 column5">20</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Cardio Blast</td>
+									<td class="cell100 column2">Gym</td>
+									<td class="cell100 column3">5:00 PM - 7:00 PM</td>
+									<td class="cell100 column4">Randy Porter</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Pilates Reformer</td>
+									<td class="cell100 column2">Gym</td>
+									<td class="cell100 column3">8:00 AM - 9:00 AM</td>
+									<td class="cell100 column4">Randy Porter</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Supple Spine and Shoulders</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">6:30 AM - 8:00 AM</td>
+									<td class="cell100 column4">Randy Porter</td>
+									<td class="cell100 column5">15</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Yoga for Divas</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">9:00 AM - 11:00 AM</td>
+									<td class="cell100 column4">Donna Wilson</td>
+									<td class="cell100 column5">20</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Virtual Cycle</td>
+									<td class="cell100 column2">Gym</td>
+									<td class="cell100 column3">8:00 AM - 9:00 AM</td>
+									<td class="cell100 column4">Randy Porter</td>
+									<td class="cell100 column5">20</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Like a butterfly</td>
+									<td class="cell100 column2">Boxing</td>
+									<td class="cell100 column3">9:00 AM - 11:00 AM</td>
+									<td class="cell100 column4">Aaron Chapman</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Mind & Body</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">8:00 AM - 9:00 AM</td>
+									<td class="cell100 column4">Adam Stewart</td>
+									<td class="cell100 column5">15</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Crit Cardio</td>
+									<td class="cell100 column2">Gym</td>
+									<td class="cell100 column3">9:00 AM - 10:00 AM</td>
+									<td class="cell100 column4">Aaron Chapman</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Wheel Pose Full Posture</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">7:00 AM - 8:30 AM</td>
+									<td class="cell100 column4">Donna Wilson</td>
+									<td class="cell100 column5">15</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Playful Dancer's Flow</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">8:00 AM - 9:00 AM</td>
+									<td class="cell100 column4">Donna Wilson</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Zumba Dance</td>
+									<td class="cell100 column2">Dance</td>
+									<td class="cell100 column3">5:00 PM - 7:00 PM</td>
+									<td class="cell100 column4">Donna Wilson</td>
+									<td class="cell100 column5">20</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Cardio Blast</td>
+									<td class="cell100 column2">Gym</td>
+									<td class="cell100 column3">5:00 PM - 7:00 PM</td>
+									<td class="cell100 column4">Randy Porter</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Pilates Reformer</td>
+									<td class="cell100 column2">Gym</td>
+									<td class="cell100 column3">8:00 AM - 9:00 AM</td>
+									<td class="cell100 column4">Randy Porter</td>
+									<td class="cell100 column5">10</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Supple Spine and Shoulders</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">6:30 AM - 8:00 AM</td>
+									<td class="cell100 column4">Randy Porter</td>
+									<td class="cell100 column5">15</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Yoga for Divas</td>
+									<td class="cell100 column2">Yoga</td>
+									<td class="cell100 column3">9:00 AM - 11:00 AM</td>
+									<td class="cell100 column4">Donna Wilson</td>
+									<td class="cell100 column5">20</td>
+								</tr>
+
+								<tr class="row100 body">
+									<td class="cell100 column1">Virtual Cycle</td>
+									<td class="cell100 column2">Gym</td>
+									<td class="cell100 column3">8:00 AM - 9:00 AM</td>
+									<td class="cell100 column4">Randy Porter</td>
+									<td class="cell100 column5">20</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+</body>
+</html>
+"""
+
+message.attach(MIMEText(mail_content, 'plain'))
+message.attach(MIMEText(html, 'html'))
+session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+session.starttls() #enable security
+session.login(sender_address, sender_pass) #login with mail_id and password
+text = message.as_string()
+session.sendmail(sender_address, receiver_address, text)
+session.quit()
+print('Mail Sent')
